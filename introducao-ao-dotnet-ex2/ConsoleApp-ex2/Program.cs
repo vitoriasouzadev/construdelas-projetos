6e9495ec28f14855bacd5bdfcd9788ed1cfe5361
vitoriasouzadev <92633829+vitoriasouzadev@users.noreply.github.com>
2022-07-18 19:41:54 -0300

    Add files via upload

---
+++ introducao-ao-dotnet-ex2/ConsoleApp-ex2/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Console.WriteLine("Hello, World!");
+//int a = 10;
+//int b = 10;
+//Console.WriteLine(a + b);
+//int c = a + b;
+//Console.WriteLine(c);
+//Console.WriteLine("Outra Coisita");
+
+
+uint ui = 100;
+float fl = 10.2f;
+long l = 45755452222222;
+ulong ul = 45755452222222;
+double d = 11452222.555;
+decimal mon = 1000.15m;
+string meuNome = "Vitória";
+char primeiraLetraDoMeuNome = 'V';
+Console.WriteLine(ui);
+Console.WriteLine(fl);
+Console.WriteLine(ul);
+Console.WriteLine(d);
+Console.WriteLine(mon);
+Console.WriteLine(meuNome);
+Console.WriteLine(primeiraLetraDoMeuNome);
+ 
---
+++ introducao-ao-dotnet-ex3-conversao/conversoes-ex/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Conversão Implícita 
+int a = 10;
+long b = a;
+Console.WriteLine(a);
+
+//Conversão Explícita (Cast) 
+double c = 10.555;
+Console.WriteLine(c);
+int d = (int) c;
+Console.WriteLine(d);
+
+//Convert Class = que precisam de uma class para realizar a conversão
+string inteiroPossivel = "1234";
+int cont = Convert.ToInt32(inteiroPossivel);
+Console.WriteLine(cont);
+
+//TryParse = Converter um tipo para outro
+int numero = 0;
+string numeroString = "1234";
+Console.WriteLine(int.TryParse(numeroString, out
+numero));
+Console.WriteLine(numero);
+
+// Acabei concatenando depois dei uma espiada na resposta e minha logica tava certa. 
+Console.WriteLine("Digite um número:");
+string a = Console.ReadLine();
+Console.WriteLine("Digite mais um número:");
+string b = Console.ReadLine();
+int c = Convert.ToInt32(a) + Convert.ToInt32(b);
+//int c = a + b;
+Console.WriteLine(c);
+
+//Conversão de tipos, entrada de dados e soma
+string a = Console.ReadLine();
+string b = Console.ReadLine();
+int aConvertido, bConvertido;
+int.TryParse(a, out aConvertido);
+int.TryParse(b, out bConvertido);
+int c = aConvertido + bConvertido;
+Console.WriteLine(c);
